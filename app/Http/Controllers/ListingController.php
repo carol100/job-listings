43aa0356174b572f430a5f0695bf40817708daa5
caroline <caroline@turelabs.com>
2022-09-17 23:45:27 +0300

    Controllers : ListingController

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    public function index()
+    {
+        return view('listings.index', [
+            'heading' => 'Latest Listings',
+            'listings' => Listing::all(),
+        ]);
+    }
+    //implement Route-Model Binding
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+}
