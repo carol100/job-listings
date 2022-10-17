577ee09f7121f0264a848e2e2fd30a02b3357c7d
Caroline Njeri <carolinenjeri100@gmail.com>
2022-10-17 23:31:41 +0300

    feat: model relationships

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -37,4 +37,10 @@ class Listing extends Model
                 ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
+
+    //relationship to user
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
